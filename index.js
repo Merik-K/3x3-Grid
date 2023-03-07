d9b1953ca1a4d48340dd03deebeaae4853530864
MerikKuhn <mkuhn181@west-mec.org>
2023-03-06 18:14:33 -0700

    fixed my code and now displays grid

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const test = document.getElementById('test');
 for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
-    var uno = document.createElement('div');
+    const box = document.createElement('div');
     box.style.height = '100px';
     box.style.width = '100px';
     box.style.border = '5px solid hotpink';
-    box.style.display = 'inline flex';
+    box.style.display = 'inline-block';
     box.onmouseover = function () {
       box.style.background = 'red';
     };
@@ -13,5 +13,13 @@ for (let i = 0; i < 3; i++) {
       box.style.background = 'white';
     };
     test.appendChild(box);
+    box.addEventListener('click', function() {
+      this.style.background = 'green'
+      box.onmouseleave = function (){ box.style.background = 'green'};
+      box.onmouseenter = function(){box.style.background = 'green'};
+    })
   }
+  const divider = document.createElement('br');
+  test.appendChild(divider);
 }
+
